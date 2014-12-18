8457c8d94895ddb14442aaa8cd0914e599c0d043
chuangwang <chuang@zhihu.com>
2014-12-18 12:06:01 +0800

    修改timeout，处理服务器端异常

---
+++ util.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import logging
+
+xtcp_logger = logging.getLogger("XTCP.access")
+xtcp_logger.setLevel(logging.DEBUG)
+
+# stream_handler = logging.StreamHandler()
+# stream_handler.setLevel(logging.DEBUG)
+
+# formatter = logging.Formatter("%(name)s:%(levelname)s: %(message)s")
+
+# stream_handler.setFormatter(formatter)
+# xtcp_logger.addHandler(stream_handler)
+
+
+class XTCPRequestContentException(Exception):
+    pass
+
+
+class XTCPHandleRequestTimeoutException(Exception):
+    pass
+
+
+class Storage(dict):
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
