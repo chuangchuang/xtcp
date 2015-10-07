a483d2f6faeadf601e6b3fe2c3bab19b71354d88
chuangwang <nameshuiliang@gmail.com>
2015-10-07 17:33:40 +0800

    modify client handler

--- pyxtcp/client_handler.py
+++ pyxtcp/client_handler.py
@@ -4,6 +4,8 @@
 import functools
 import json
 
+from tornado import gen
+
 from .util import CONNECTION_TYPE_IN_REQUEST, RESPONSE_ERROR_TAG
 from .util import RPCMessage, Storage, log
 from .simple_client import RPCClientItem
@@ -22,6 +24,7 @@ class _BasicRPCClientServiceHandler(object):
         self._client = client
         self._service_name = service_name
 
+    @gen.coroutine
     def ___handler_request(self, func_name, **kwargs):
         topic_name = "{}.{}".format(self._service_name, func_name)
         body = ""
@@ -31,47 +34,25 @@ class _BasicRPCClientServiceHandler(object):
         log.debug("{}({})".format(topic_name, body))
         request_message = RPCMessage(CONNECTION_TYPE_IN_REQUEST, topic_name, body)
 
-        response_message = Storage()
-
         def _f(_message):
             log.debug("Request Message {}".format(_message.__dict__))
 
             status = _message.topic
             content = _message.body
             if status == RESPONSE_ERROR_TAG:
-                raise content
+                raise gen.Return(content)
 
             if not content:
-                response_message.v = content
-                return
+                raise gen.Return(content)
 
+            v = content
             try:
-                response_message.v = Storage(json.loads(content))
+                v = Storage(json.loads(content))
             except:
-                response_message.v = content
-            return
+                v = content
+            raise gen.Return(v)
 
-        self._client.fetch(RPCClientItem(request_message, _f))
-        return response_message.v
-
-    def ___handler_response(self, message):
-        log.debug("Request Message {}".format(message.__dict__))
-
-        status = message.topic
-        content = message.body
-        if status == RESPONSE_ERROR_TAG:
-            raise content
-
-        if not content:
-            return content
-
-        json.loads(content)
-
-        try:
-            log.warn(("*******", content))
-            return Storage(json.loads(content))
-        except:
-            return content
+        yield self._client.fetch(RPCClientItem(request_message, _f))
 
     def __getattr__(self, func):
         try:
